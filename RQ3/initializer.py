2ce0a992200241e5d8935f3adfb3fc1bb33bad0e
ZhuTiangang <112457131+ZhuTiangang@users.noreply.github.com>
2022-09-01 10:55:14 +0800

    Add files via upload

--- RQ3/initializer.py
+++ RQ3/initializer.py
@@ -1,15 +1,16 @@
-import tensorflow as tf
+import tensorflow._api.v2.compat.v1 as tf
+tf.disable_v2_behavior()
 import numpy as np
 from keras import backend as K
 if __name__ == '__main__':
 
     dataset = 'mnist'
-    model_name = 'lenet4'
+    model_name = 'lenet1'
 
     num = 5000
     epoch = 20
-    seed = 42
-    R = 2
+    seed = 46
+    R = 1
 
     from keras.models import load_model
 
@@ -18,4 +19,16 @@ if __name__ == '__main__':
     tf.compat.v1.disable_eager_execution()
     sess = tf.compat.v1.InteractiveSession()
     sess.run(tf.compat.v1.global_variables_initializer())
+
+    # def init_layer(layer):
+    #     session = K.get_session()
+    #     weights_initializer = tf.variables_initializer(layer.weights)
+    #     session.run(weights_initializer)
+    #
+    # for layer in model.layers:
+    #     init_layer(layer)
+
+    import os
+    os.makedirs('new_model/' + dataset + '/' + model_name + '/{}'.format(R),exist_ok=True)
+
     model.save('new_model/' + dataset + '/'+ model_name + '/{}/init_model.h5'.format(R))
